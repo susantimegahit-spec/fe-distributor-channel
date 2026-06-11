fab5a606d612b2737372c7085170808b6d651b04
troy kawengian <54972947+troykawengian77@users.noreply.github.com>
2026-06-11 15:14:33 +0700

    Update OrderList.jsx

--- src/views/order/OrderList.jsx
+++ src/views/order/OrderList.jsx
@@ -110,7 +110,7 @@ export default function OrderList() {
           </Stack>
         }
         secondary={
-          <Button variant="primary" href="/order/order-create">
+          <Button variant="primary" as={Link} to={`/order/order-create`}>
             <i className="ti ti-plus me-1" />
             Tambah Order
           </Button>
